4ac45ca0076d9b0dcc89457a4b64dff50b6012fb
Jay Kan <leicasper@gmail.com>
2015-10-30 12:26:23 -0700

    Add asynchronous test assertion implemenation

---
+++ chapter_2/asynchronous_test_assertion.js
@@ -0,0 +1,42 @@
+;(function(){
+
+  var queue  = [],
+      paused = false,
+      results;
+
+  this.test = function( name, fn ){
+    queue.push(function(){
+      results = document.getElementById( 'results' );
+      results = assert(true, name).appendChild(
+        document.createElement( 'ul' )
+      );
+      fn();
+    });
+    runTest();
+  };
+  this.pause = function(){
+    paused = true;
+  };
+  this.resume = function(){
+    paused = false;
+    setTimeout(runTest, 1);
+  };
+  function runTest(){
+    if ( !paused && queue.length ){
+      queue.shift()();
+      if ( !paused ){
+        resume();
+      }
+    }
+  }
+  this.assert = function( value, description ){
+    var li = document.createElement( 'li' );
+    li.className = value ? 'pass' : 'fail';
+    li.appendChild( document.createTextNode(description) );
+    results.appendChild( li );
+    if ( !value ){
+      li.parentNode.parentNode.className = 'fail';
+    }
+    return li;
+  }
+})();
