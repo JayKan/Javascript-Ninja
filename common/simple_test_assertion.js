1652be3cca13de8484d91d8aa901f37770736fae
Jay Kan <leicasper@gmail.com>
2015-11-03 16:32:15 -0800

    Add Ajax closure example

--- common/simple_test_assertion.js
+++ common/simple_test_assertion.js
@@ -1,6 +1,12 @@
 function assert( value, description ){
+  var resultsList = document.getElementById( 'results' );
+  if ( !resultsList ){
+    resultsList = document.createElement( 'ul' );
+    document.getElementsByTagName('body')[0].appendChild( resultsList );
+    resultsList.setAttribute('id', 'results');
+  }
   var li = document.createElement( 'li' );
   li.className = value ? 'pass' : 'fail';
   li.appendChild( document.createTextNode(description) );
-  document.getElementById('results').appendChild( li );
+  resultsList.appendChild( li );
 }
