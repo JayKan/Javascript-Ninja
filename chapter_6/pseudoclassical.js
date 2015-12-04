b49c332bf6dbe12706e203b188db3cb02fdcca22
Jay Kan <leicasper@gmail.com>
2015-12-03 16:06:56 -0800

    Overwriting existing prototype instance method

--- chapter_6/pseudoclassical.js
+++ chapter_6/pseudoclassical.js
@@ -43,4 +43,40 @@
 
   assert( name === 'Herb the Mammal', 'Pseudoclassical Example 1: Name should be the same!');
 
+  // Let's make a Cat that inherits from Mammal
+  var Cat = function(name) {
+    this.name = name;
+    this.saying = 'meow';
+  };
+
+  // Replace Cat.prototype with a new instance of Mammal
+  Cat.prototype = new Mammal();
+
+  // Augment the new prototype with
+  // purr and getName methods.
+  Cat.prototype.purr = function(n) {
+    var i, s = '';
+    for (i = 0; i < n; i += 1) {
+      if (s) {
+        s += '-';
+      }
+      s += 'r';
+    }
+    return s;
+  };
+  Cat.prototype.getName = function(){
+    return this.says() + ' ' + this.name + ' ' + this.says();
+  };
+
+  var myCat = new Cat('Henrietta');
+  var says = myCat.says();
+  assert( says === 'meow', 'Pseudoclassical Example 2: MyCat should says meow');
+
+  var purr = myCat.purr(5);
+  assert( purr === 'r-r-r-r-r', 'Pseudoclassical Example 3: MyCat should purr r-r-r-r-r-r.');
+
+  var catName = myCat.getName();
+  assert( catName === 'meow Henrietta meow', 'Pseudoclassical Example 3: myCat overrides sayName() prototype method.');
+
+
 })();
