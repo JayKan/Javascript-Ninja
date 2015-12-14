06fb3d7c7ab0331c64f6d9f2c72d7fda8e340362
Jay Kan <leicasper@gmail.com>
2015-12-14 15:20:53 -0800

    Add (The Command Pattern) examples

--- chapter_6/constructor.pattern.js
+++ chapter_6/constructor.pattern.js
@@ -158,4 +158,35 @@
   console.log(myNewCar.toString() );
 
 
+  // ----------------------------------------------------------------------------------- //
+  // ----------------------------- The Command Pattern --------------------------------- //
+  // ----------------------------------------------------------------------------------- //
+  /*
+    The Command Pattern aims to encapsulate method invocation, requests or operations into a
+    single object and gives us the ability to both parameterize and pass method calls around
+    that can be executed at our discretion.
+   */
+  (function(){
+
+    var carManager = {
+
+      // request information
+      requestInfo: function( model, id ) {
+        return 'The information for ' + model + ' with ID ' + id + ' is foobar.';
+      },
+
+      // purchase the car
+      buyVehicle: function( model, id ) {
+        return 'You have successfully purchased Item ' + id + ', a ' + model;
+      },
+
+      // arrange a viewing
+      arrangeViewing: function( model, id ) {
+        return 'You have successfully booked a viewing of ' + model + ' (' + id + ' ) ';
+      }
+    };
+  })();
+
+
+
 })();
