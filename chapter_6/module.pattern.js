d42c510a9684ce105bac9f5f07e86a96c632e3a7
Jay Kan <leicasper@gmail.com>
2015-12-14 12:17:57 -0800

    Add (The Observer Pattern) examples

--- chapter_6/module.pattern.js
+++ chapter_6/module.pattern.js
@@ -397,4 +397,141 @@
   assert( singletonTest.pointX === 5,
   'The Singleton Pattern: singletonTest.pointX should === ' + singletonTest.pointX);
 
+  // ------------------------------------------------------------------------ //
+  // ---------------------- The Observer Pattern ---------------------------- //
+  // ------------------------------------------------------------------------ //
+  /*
+    The Observer is a design pattern when an object (known as a subject) maintains a list
+    of objects depending on it (observers), automatically notifying them of any changes to state.
+
+     - Subject: maintains a list of observers, facilitates adding or removing observers.
+     - Observer: provides a update interface for objects that need to be notified of a Subject's changes of state.
+     - ConcreteSubject: broadcasts notifications to observers on changes of state, stores the state of ConcreteObservers.
+     - ConcreteObserver: stores a reference to the ConcreteSubject, implements an update interface for the Observer to ensure
+       state is consistent with the Subjects.
+   */
+
+  // First, let's model the list of dependent Observers a subject may have:
+  function ObserverList() {
+    this.observerList = [];
+  }
+  ObserverList.prototype.add = function( obj ) {
+    return this.observerList.push( obj );
+  };
+  ObserverList.prototype.count = function() {
+    return this.observerList.length;
+  };
+  ObserverList.prototype.get = function( index ) {
+    if ( index > -1 && index < this.observerList.length ) {
+      return this.observerList[ index ];
+    }
+  };
+  ObserverList.prototype.indexOf = function( obj, startIndex ){
+    var i  = startIndex;
+    while ( i < this.observerList.length ) {
+      if ( this.observerList[i] === obj ) {
+        return i;
+      }
+      i++;
+    }
+    return -1;
+  };
+  ObserverList.prototype.removeAt = function( index ){
+    this.observerList.splice( index, 1 );
+  };
+
+  // Next, let's model the Subject and the ability to add,
+  // remove, or notify observers on the observer list.
+  function Subject() {
+    this.observers = new ObserverList();
+  }
+  Subject.prototype.addObserver = function( observer ) {
+    this.observers.add( observer );
+  };
+  Subject.prototype.removeObserver = function( observer ) {
+    this.observers.removeAt( this.observers.indexOf( observer, 0) );
+  };
+  Subject.prototype.notify = function( context ) {
+    var observerCount = this.observers.count();
+    for (var i = 0; i < observerCount; i++ ) {
+      this.observers.get(i).update( context );
+    }
+  };
+
+  // The Observer
+  function Observer() {
+    this.update = function() {};
+  }
+
+
+  // Extend an object with an extension
+  function extend( object, extension ) {
+    for ( var key in extension ) {
+      object[key] = extension[key]
+    }
+  }
+
+  // References to our DOM Elements.
+  var controlCheckBox = document.getElementById( 'mainCheckbox' ),
+      addButton       = document.getElementById( 'addNewObserver'),
+      container       = document.getElementById( 'observersContainer');
+
+  // Concrete Subject
+
+  // Extend the controlling checkbox with the new Subject Class.
+  extend( controlCheckBox, new Subject() );
+
+  // Clicking the checkbox will trigger notifications to its observers.
+  controlCheckBox.onclick = function() {
+    controlCheckBox.notify( controlCheckBox.checked );
+  };
+
+  addButton.onclick = addNewObserver;
+
+  // Concrete Observer
+  function addNewObserver() {
+
+    // Create a new checkbox to be added
+    var check = document.createElement( 'input' );
+    check.type = 'checkbox';
+
+    // Extend the checkbox with the Observer Class
+    extend( check, new Observer() );
+
+    // Override with custom update behaviour
+    check.update = function( value ){
+      this.checked = value;
+    };
+
+    // Add the new observer to our list of observers
+    // for our main subject.
+    controlCheckBox.addObserver( check );
+
+    // Append the item to the container
+    container.appendChild( check );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })(window.jQuery, window._);
