e672dbd011eecc22510b007a5f0968f0f9f4c767
Jay Kan <leicasper@gmail.com>
2015-12-17 11:43:58 -0800

    Add (The Flyweight Pattern) examples

--- chapter_6/module.pattern.js
+++ chapter_6/module.pattern.js
@@ -1037,6 +1037,145 @@
   'jQuery Decorators Example: ' + decoratorApp.printObj(decoratorApp.defaults));
 
 
+  // ----------------------------------------------------------------------------------- //
+  // --------------------------- The Flyweight Pattern  -------------------------------- //
+  // ----------------------------------------------------------------------------------- //
+  /*
+    The Flyweight Pattern is a classical structural solution for optimizing code that is repetitive,
+    slow and inefficiently shares data. It aims to minimize the use of memory in an application by
+    sharing as much data as possible with related objects.
 
+    Two ways in which the Flyweight pattern can be applied:
+      1) At the data-layer, where we deal with the concept of sharing data between large quantities of
+        similar objects stored in memory.
+      2) At the DOM-layer, where the Flyweight can be used as a central event-manager to avoid attaching
+        event handlers to every child element in a parent container.
+   */
+  // Simulate pure virtual inheritance/"implement" keyword for JS
+  Function.prototype.implementsFor = function(parentClassOrObject) {
+    if (parentClassOrObject.constructor === Function) {
+      // Normal inheritance
+      this.prototype = new parentClassOrObject();
+      this.prototype.constructor = this;
+      this.prototype.parent = parentClassOrObject.prototype;
+    }
+    else {
+      // Pure Virtual Inheritance
+      this.prototype = parentClassOrObject;
+      this.prototype.constructor = this;
+      this.prototype.parent = parentClassOrObject;
+    }
+    return this;
+  };
+
+  // Flyweight object
+  var CoffeeOrder = {
+    // Interfaces
+    serveCoffee: function(context) {},
+    getFlavor: function(){}
+  };
+
+  // ConcreteFlyweight object that creates ConcreteFlyweight
+  // Implements CoffeeOrder
+  function CoffeeFlavor(newFlavor) {
+    var flavor = newFlavor;
+
+    // If an interface has been defined for a feature
+    // implement the feature
+    if (typeof this.getFlavor === 'function') {
+      this.getFlavor = function(){
+        return flavor;
+      }
+    }
+
+    if (typeof this.serveCoffee === 'function') {
+      this.serveCoffee = function(context) {
+        console.log('Serving Coffee flavor '
+          + flavor
+          + ' to table number '
+          + context );
+      }
+    }
+  }
+
+  // Implement interface for CoffeeOrder
+  CoffeeFlavor.implementsFor( CoffeeOrder );
+
+  // Handle table numbers for a coffee order
+  function CoffeeOrderContext( tableNumber) {
+    return {
+      getTable: function(){
+        return tableNumber;
+      }
+    }
+  }
+
+  // Create a CoffeeFlavorFactory
+  function CoffeeFlavorFactory() {
+    var flavors = {},
+        length  = 0;
+    return {
+      getCoffeeFlavor: function(name) {
+        var flavor = flavors[name];
+        if (typeof flavor === 'undefined') {
+          flavor = new CoffeeFlavor(name);
+          flavors[name] = flavor;
+          length += 1;
+        }
+        return flavor;
+      },
+
+      getTotalCoffeeMade: function(){
+        return length;
+      }
+    };
+  }
+
+  // Sample usage: testFlyweight()
+  function testFlyweight() {
+
+    // The flavors ordered.
+    var flavors = new CoffeeFlavor();
+
+    // The tables for the orders.
+    var tables = new CoffeeOrderContext();
+
+    // Number of orders made
+    var ordersMade = 0;
+
+    // The CoffeeFlavorFactory instance
+    var flavorFactory;
+
+    function takeOrders( flavorIn ,table) {
+      flavors[ordersMade] = flavorFactory.getCoffeeFlavor( flavorIn );
+      tables[ordersMade+=1] = new CoffeeOrderContext( table );
+    }
+
+    flavorFactory = new CoffeeFlavorFactory();
+
+    takeOrders('Cappuccino', 2);
+    takeOrders('Cappuccino', 2);
+    takeOrders('Frappe', 1);
+    takeOrders('Frappe', 1);
+    takeOrders('Xpresso', 1);
+    takeOrders('Frappe', 897);
+    takeOrders('Cappuccino', 97);
+    takeOrders('Cappuccino', 97);
+    takeOrders('Frappe', 3);
+    takeOrders('Xpresso', 3);
+    takeOrders('Cappuccino', 3);
+    takeOrders('Xpresso', 96);
+    takeOrders('Frappe', 552);
+    takeOrders('Cappuccino', 121);
+    takeOrders('Xpresso', 121);
+
+    for (var i = 0; i < ordersMade; i+=1) (function(i){
+      flavors[i].serveCoffee(tables[i]);
+    })(i)
+
+    console.log('total CoffeeFlavor Objects made: ' + flavorFactory.getTotalCoffeeMade() );
+  }
+
+  testFlyweight();
 
 })(window.jQuery, window._);
